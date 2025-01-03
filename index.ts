362ca11c7e91875e3e1c3348ef1058292bbb9c1b
sachinduKavi <cst21017@std.uwu.ac.lk>
2025-01-03 21:17:43 +0530

    fixxx

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import express, {Express} from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import vernamRouter from './route/vernam'
 
 dotenv.config()
 
+
 const app: Express = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
 app.use(express.json())
 
 
